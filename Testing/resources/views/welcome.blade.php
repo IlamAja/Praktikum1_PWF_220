5b96abe6212c0e6dc2737f616bd6455bab53ed7b
Ilam Ball <informasiilam@gmail.com>
2026-02-27 09:19:15 +0700

    first commit

---
+++ Testing/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tugas Laravel</title>
+</head>
+<body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; background-color: #f3f4f6;">
+    <div style="background: white; padding: 2rem; border-radius: 10px; shadow: 0 10px 15px rgba(0,0,0,0.1); text-align: center;">
+        <h2>Nama: Muhammad Islam</h2>
+        <h2>NIM: 20230140220</h2>
+    </div>
+</body>
+</html>
